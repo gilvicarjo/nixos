24cd138a7d9e0a0e19eed868a08ec84ade17a7ae
Gil Araujo <gil.araujo_ext@a-to-be.com>
2025-03-10 06:05:25 +0000

    nix config

---
+++ modules/nix/helm/default.nix
@@ -0,0 +1,18 @@
+home.file = {
+  ".config/helm/repositories.yaml" = {
+    force = true;
+    text = ''
+       apiversion: **
+       generated: "0001-01-01T00:00:00Z"
+       repositories:
+       - caFile: **
+        certFile: **
+        insecure_skip_tls_verify: false
+        keyFile: **
+        name: temporal
+        pass_credentials_all: false
+        password: **
+        url: https://go.temporal.io/helm-charts
+        username: **
+        };
+}
